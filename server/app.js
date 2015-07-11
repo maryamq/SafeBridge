1712d2e319c31da99b9e3784c1c992198bc099e5
Akshay <akshay.pushparaja@gmail.com>
2015-07-11 13:34:45 -0700

    sample node.js code

---
+++ server/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/api/v1/serviceName', function(req, res){
+  /*
+  service code here
+  //accessing get params
+    req.params.id
+  */
+  res.send(response);
+});
+
+app.post('/api/v1/serviceName', function(req, res){
+  /*
+  service code here
+  //accessing post params
+    req.body
+  */
+  res.send(response);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 3000);
