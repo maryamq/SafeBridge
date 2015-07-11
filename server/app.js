581b3b3ec0df777649f8cad339ab50d2df9e8397
Maryam Quadir <maryamquadir@gmail.com>
2015-07-11 16:37:42 -0700

    Added phone has functions

--- server/app.js
+++ server/app.js
@@ -36,11 +36,12 @@ client.parse = (function() {
   var appId = "EskNAzWggrRsCs2y0BFSEGUoHoNhzOASNFfqGVi9",
       jsKey = "2X5eB9njA4iVStOfhnk69y7kggwxJ6MCviEPQW2T",
       Client_Map  = Parse.Object.extend("client_map"),
-      getPhoneHash;
+      getPhoneHash, generatePhoneHash, getPhoneFromHash, hashCode;
 
   // Initialize Parse
   Parse.initialize(appId, jsKey);
 
+  // Returns phone hash code from the db.
   getPhoneHash = function(phone_num, onSuccess) {
     var query = new Parse.Query(Client_Map);
     query.equalTo("ph_num", phone_num);
@@ -49,12 +50,63 @@ client.parse = (function() {
         if (result && result.length  > 0) {
           phone_code = result[0].attributes.uq_code;
           onSuccess(phone_code);
+        } else {
+          onSuccess("");
         }
     });
   };
 
+  // Returns phone hash code from the db.
+  getPhoneFromHash = function(phone_hash, onSuccess) {
+    var query = new Parse.Query(Client_Map);
+    query.equalTo("uq_code", phone_hash);
+    var phone_code = "";
+    query.find().then(function(result){
+        if (result && result.length  > 0) {
+          phone_num = result[0].attributes.ph_num;
+          onSuccess(phone_num);
+        } else {
+          onSuccess("");
+        }
+    });
+  };
+
+  // Generate hash code for the phone num and stores in db
+  generatePhoneHash = function(phone_num, onSuccess) {
+    var hash = hashCode(phone_num);
+    var new_client = new Client_Map();
+    new_client.set("ph_num" , phone_num);
+    new_client.set("uq_code", hash + "");
+    new_client.save(null, {
+      success: function(new_client) {
+        // Execute any logic that should take place after the object is saved.
+        onSuccess(new_client.id);
+        console.log('New object created with objectId: ' + new_client.id);
+      },
+      error: function(new_client, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        onSuccess("");
+        console.log('Failed to create new object, with error code: ' + error.message);
+      }
+    });
+  };
+
+  hashCode = function(str) {
+    var hash = 0, i, chr, len;
+    if (str.length == 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
+ 
   return {
-    getPhoneHash : getPhoneHash
+    getPhoneHash : getPhoneHash,
+    generatePhoneHash: generatePhoneHash,
+    getPhoneFromHash: getPhoneFromHash
   };
 
 
@@ -73,6 +125,9 @@ app.get('/api/v1/sendMessage', function(req, res){
   // look up the receiver chat id.
 
   var response = "hello world";
+  client.parse.getPhoneFromHash("abc", function(value) {
+    console.log("success " + value);
+  })
   res.send(response)
 });
 
@@ -90,7 +145,6 @@ app.post('/api/v1/serviceName', function(req, res){
   */
   res.send(response);
 });
-
 
 
 app.use(express.static(__dirname + '/public'));
