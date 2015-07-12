b7742f44f90b2e1160260649e57f1efb22c32ae2
Kevin Berry <berry.kevinw@gmail.com>
2015-07-11 17:05:48 -0700

    added new parse backend

--- server/app.js
+++ server/app.js
@@ -1,34 +1,20 @@
-var express = require('express');
-var app = express();
-var bodyParser = require("body-parser");
+var express        = require('express');
+var app            = express();
+var bodyParser     = require("body-parser");
+var Parse          = require('node-parse-api').Parse;
+
+var options = {
+    app_id: process.env.ParseAppID,
+    api_key: process.env.ParseSecretKey
+}
+
+var parseBackend = new Parse(options);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var client = {}
-client.twilio = (function() {
-  // Twilio Credentials 
-  var accountSid = process.env.twilioSid,
-      authToken = process.env.twilioToken,
-      fromPhone =  '+19783636041',
-      twilio = require('twilio')(accountSid, authToken),
-      sendMessage, init;
-
-
-  sendMessage = function(toPhone, message) {
-    twilio.messages.create({
-      from: fromPhone,  
-      to: toPhone,
-      body: message
-    });
-  };
-  
-  return {
-    sendMessage : sendMessage
-  };
-
-}());
- 
+client.twilio = require('twilio')(process.env.twilioSid, process.env.twilioToken);
 
 app.get('/api/v1/sendMessage', function(req, res){
   /*
@@ -36,14 +22,27 @@ app.get('/api/v1/sendMessage', function(req, res){
   //accessing get params
     req.params.id
   */
-  var chat_id = req.param.chat_id;  // id of the session
-  var message = req.param.message;
+  var uniqueId  = req.param.unique_id;  // id of the session
+  var smsMessage   = req.param.message;
 
-  // look up the receiver chat id.
-
-  var response = "hello world";
-  client.twilio.sendMessage("+16039186391", "This is Maryam!");
-  res.send(response);
+  parseBackend.find('client_map', {uq_code: uniqueId}, function (err, response) {
+    if(!err) {
+      var phoneNumber = ("+1" + response.ph_num)
+      client.twilio.sendMessage({
+          to: phoneNumber,
+          from: '+19783636041',
+          body: message
+      }, function(err, responseData) {
+          if (!err) {
+            res.send("success");
+          } else {
+            throw new Error('There was an issue sending the message to the client.')
+          }
+      });
+    } else {
+      throw new Error('The unique_id ' + uniqueId + ' could not be found.')
+    }
+  });
 });
 
 app.get('/api/v1/endConversation', function(req, res){
